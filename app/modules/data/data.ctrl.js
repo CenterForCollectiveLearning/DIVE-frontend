1618b5a2b541e4f3a88035e9826db537a8c1654b
Kevin Hu <kzh@mit.edu>
2015-07-01 16:38:26 -0400

    Removing ngHandsontable, too much of a hassle

--- app/modules/data/data.ctrl.js
+++ app/modules/data/data.ctrl.js
@@ -1,6 +1,5 @@
 var _ = require('underscore');
 require('handsontable');
-require('ngHandsontable');
 
 var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
