fbdb2ca50ce8873eb0ab0904adad38feb02181de
Kevin Hu <kzh@mit.edu>
2015-07-17 12:41:43 -0400

    Making project tabs work on landing again

--- app/modules/project/project_tabs.ctrl.js
+++ app/modules/project/project_tabs.ctrl.js
@@ -5,25 +5,30 @@ angular.module('diveApp.project').controller('ProjectTabsCtrl', function($scope,
   $scope.tabs = [
     {
       state: 'project.data.upload',
+      baseState: 'project.data',
       label: 'Datasets'
     }, {
       state: 'project.visualize.builder',
+      baseState: 'project.visualize',
       label: 'Visualizations'
     }, {
       state: 'project.analysis.manual',
+      baseState: 'project.analysis',
       label:' Analysis'
     }, {
       state: 'project.export',
+      baseState: 'project.export',
       label: 'Export'
     }, {
       state: 'project.overview',
+      baseState: 'project.overview',
       label: 'Overview'
     }
   ];
 
   var currentStateSplit = $state.current.name.split('.');
   var currentState = currentStateSplit[0] + '.' + currentStateSplit[1];
-  $scope.selectedIndex = _.findIndex($scope.tabs, { state: currentState });
+  $scope.selectedIndex = _.findIndex($scope.tabs, { baseState: currentState });
   $scope.$watch('selectedIndex', function(current, old) {
     if (current >= 0 && old >= 0 && current != old) {
       $state.go($scope.tabs[current].state);
