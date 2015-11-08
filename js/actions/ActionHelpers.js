2e793ff06d50a39940ffa3f64a5bb9b5b5972ab7
Guru Mahendran <me@gurubavan.com>
2015-11-07 19:47:34 -0500

    function -> lambda

--- js/actions/ActionHelpers.js
+++ js/actions/ActionHelpers.js
@@ -2,9 +2,9 @@ export function formatTableData(columnNames, data) {
   function formatRow(columns, row) {
     var newRow = {};
 
-    columns.forEach(function (column, j) {
-      newRow[`${ column.replace(/[.]/g, '_') }`] = row[j];
-    });
+    columns.forEach((column, j) =>
+      newRow[`${ column.replace(/[.]/g, '_') }`] = row[j]
+    );
 
     return newRow;
   }
