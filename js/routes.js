023ca4aa1bbeab0023b8823569c9b8ddd8380413
Guru Mahendran <me@gurubavan.com>
2015-11-12 17:56:22 -0500

    /transform: remove /transform

--- js/routes.js
+++ js/routes.js
@@ -11,7 +11,6 @@ import ProjectsPage from './components/ProjectsPage';
 import DatasetsPage from './components/Datasets/DatasetsPage';
 import DatasetUploadPage from './components/Datasets/DatasetUploadPage';
 import DatasetInspectPage from './components/Datasets/DatasetInspectPage';
-import TransformPage from './components/Transform/TransformPage';
 import VisualizationsPage from './components/Visualizations/VisualizationsPage';
 import GalleryPage from './components/Visualizations/Gallery/GalleryPage';
 import BuilderPage from './components/Visualizations/Builder/BuilderPage';
@@ -34,7 +33,6 @@ export default (
         <Route path="upload" component={ DatasetUploadPage }/>
         <Route path=":datasetId/inspect" component={ DatasetInspectPage }/>
       </Route>
-      <Route path="transform" component={ TransformPage }/>
       <Route path="visualize" component={ VisualizationsPage }>
         <Route path="gallery" component={ GalleryPage }/>
         <Route path="builder/:specId" component={ BuilderPage }/>
