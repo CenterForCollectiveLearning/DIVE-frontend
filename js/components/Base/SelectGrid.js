4ba6a631cf12eca269b0508a6cd6c8c8fb82d13c
Guru Mahendran <me@gurubavan.com>
2015-11-17 01:18:19 -0500

    select-grid: allow for 'select some' half-checked checkbox state

--- js/components/Base/SelectGrid.js
+++ js/components/Base/SelectGrid.js
@@ -6,30 +6,49 @@ import SelectGridElement from './SelectGridElement.js';
 export default class SelectGrid extends Component {
   constructor(props) {
     super(props);
+
+    
     
     this.state = {
-      selectedAll: false
-    }
+      selected: 0,
+      selectStates: {
+        NONE: 0,
+        SOME: 1,
+        ALL: 2
+      }
+    };
   }
 
   selectAll() {
-    this.setState({ selectedAll: !this.state.selectedAll });
+    this.setState({ selected: (this.state.selected == this.state.selectStates.ALL ? this.state.selectStates.NONE : this.state.selectStates.ALL) });
 
-    const selectAll = (() => this.props.onSelectAllItems(this.state.selectedAll));
+    const selectAll = (() => this.props.onSelectAllItems(this.state.selected == this.state.selectStates.ALL));
     setTimeout(selectAll, 100);
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.items.every((item) => item.selected) != this.state.selectedAll) {
-      this.setState({ selectedAll: !this.state.selectedAll });
+    if (nextProps.items.every((item) => item.selected)) {
+      this.setState({ selected: this.state.selectStates.ALL });
+    } else if (nextProps.items.every((item) => !item.selected)) {
+      this.setState({ selected: this.state.selectStates.NONE });
+    } else {        
+      this.setState({ selected: this.state.selectStates.SOME });
     }
   }
 
   render() {
+    var checkedClass = '';
+
+    if (this.state.selected == this.state.selectStates.ALL) {
+      checkedClass = styles.checked;
+    } else if (this.state.selected == this.state.selectStates.SOME) {
+      checkedClass = styles.checked + ' ' + styles.halfChecked;
+    }
+
     return (
       <div className={ styles.selectGridContainer + (this.props.className ? ' ' + this.props.className : '') }>
         <div className={ styles.selectHeading } onClick={ this.selectAll.bind(this) }>
-          <input type="checkbox" className={ this.state.selectedAll ? styles.checked : '' } checked={ this.state.selectedAll }/>
+          <input type="checkbox" className={ checkedClass } checked={ this.state.selected == this.state.selectStates.ALL }/>
           <label>{ this.props.heading }</label>
         </div>
         <div className={ styles.selectGrid }>
