91c5ea24c42f6d0b5626a9c8a93947dd814a652f
Guru Mahendran <me@gurubavan.com>
2016-01-05 14:30:47 -0500

    set server.js for prod

--- server.js
+++ server.js
@@ -1,15 +1,48 @@
+// var webpack = require('webpack');
+// var WebpackDevServer = require('webpack-dev-server');
+// var config = require('./webpack.config');
+
+// new WebpackDevServer(webpack(config), {
+//   publicPath: config.output.publicPath,
+//   hot: true,
+//   historyApiFallback: true
+// }).listen(3009, 'localhost', function (err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+
+//   console.log('Listening at localhost:3009');
+// });
+
+var express = require('express');
+var path = require('path');
 var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var app = express();
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3009, 'localhost', function (err, result) {
-  if (err) {
-    console.log(err);
-  }
+var isDevelopment = (process.env.NODE_ENV !== 'production');
+var static_path = path.join(__dirname, 'public');
 
-  console.log('Listening at localhost:3009');
-});
+app.use(express.static(static_path))
+  .get('/', function (req, res) {
+    res.sendFile('index.html', {
+      root: static_path
+    });
+  }).listen(process.env.PORT || 8080, function (err) {
+    if (err) { console.log(err) };
+    console.log('Listening at localhost:8080');
+  });
+
+if (isDevelopment) {
+  var config = require('./webpack.config');
+  var WebpackDevServer = require('webpack-dev-server');
+
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
+  }).listen(3009, 'localhost', function (err, result) {
+    if (err) { console.log(err) }
+    console.log('Listening at localhost:3009');
+  });
+}
+
