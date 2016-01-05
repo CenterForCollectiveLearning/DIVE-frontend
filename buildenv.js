aa9862920d3809cf3fabbec32a375a988c64b4ec
Guru Mahendran <me@gurubavan.com>
2016-01-05 15:35:04 -0500

    change env.js dir

--- buildenv.js
+++ buildenv.js
@@ -7,7 +7,7 @@ var env = require('./.env.js');
 // Used for local development with webpack-dev-server
 output = util.inspect(env, {depth: null});
 output = 'window.__env = ' + output + ';\n';
-output.to('/public/env.js');
+output.to(path.join(__dirname, 'public')+'/env.js');
 
 // Used to test build with divshot server
 output = JSON.stringify(env, null, 2);
