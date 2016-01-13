dececc0d2997b99095b6fa0b1d27cb3f6cc0790e
Guru Mahendran <me@gurubavan.com>
2016-01-13 15:44:03 -0500

    base-component: Card

--- public/js/components/Analysis/Comparison/ComparisonView.js
+++ public/js/components/Analysis/Comparison/ComparisonView.js
@@ -3,10 +3,16 @@ import { connect } from 'react-redux';
 
 import styles from '../Analysis.sass';
 
+import Card from '../../Base/Card';
+import HeaderBar from '../../Base/HeaderBar';
+
 export class ComparisonView extends Component {
   render() {
     return (
       <div className={ styles.comparisonViewContainer }>
+        <Card>
+          <HeaderBar header={ <span>Comparison Table</span> } />
+        </Card>
       </div>
     );
   }
