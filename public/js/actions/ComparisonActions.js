ed0584c13b78452a052df1d76a3958870cb40fd7
Mike <mikewang@mit.edu>
2016-01-14 15:10:10 -0500

    saved results into state

--- public/js/actions/ComparisonActions.js
+++ public/js/actions/ComparisonActions.js
@@ -1,6 +1,8 @@
 import {
   SELECT_COMPARISON_AGGREGATION_VARIABLE,
-  SELECT_COMPARISON_INDEPENDENT_VARIABLE
+  SELECT_COMPARISON_INDEPENDENT_VARIABLE,
+  REQUEST_MAKE_COMPARISON,
+  RECEIVE_MAKE_COMPARISON
 } from '../constants/ActionTypes';
 
 export function selectComparisonVariable(selectedIndependentVariableId) {
@@ -17,4 +19,40 @@ export function selectAggregationVariable(selectedAggregationVariableId) {
     comparisonAggregationVariableId: selectedAggregationVariableId,
     selectedAt: Date.now()
   }
+}
+
+function requestMakeComparisonDispatcher(datasetId) {
+  return {
+    type: REQUEST_MAKE_COMPARISON
+  };
+}
+
+function receiveMakeComparisonDispatcher(json) {
+  return {
+    type: RECEIVE_MAKE_COMPARISON
+    data: json,
+    receivedAt: Date.now()
+  };
+}
+
+export function runMakeComparison(projectId, datasetId, aggregationVariable, comparisonVariables) {
+  const params = {
+    projectId: projectId,
+    spec: {
+      datasetId: datasetId,
+      numericalDependentVariable: aggregationVariable,
+      categoricalIndependentVariableNames: comparisonVariables
+    }
+  }
+
+  return (dispatch) => {
+    dispatch(requestMakeComparisonDispatcher());
+    return fetch('/statistics/v1/contingency_table', {
+      method: 'post',
+      body: JSON.stringify(params),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => response.json())
+      .then(json => dispatch(receiveMakeComparisonDispatcher(json)))
+      .catch(err => console.error("Error creating contingency table: ", err));
+  };
 }
