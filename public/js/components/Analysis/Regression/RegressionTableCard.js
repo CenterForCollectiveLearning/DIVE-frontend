dbb262001cd7151069bcc1e41673631438286d07
Guru Mahendran <me@gurubavan.com>
2016-01-20 16:58:30 -0500

    regression: break off more components and fix ContributionToRSquared graph formatting

---
+++ public/js/components/Analysis/Regression/RegressionTableCard.js
@@ -0,0 +1,32 @@
+import React, { Component, PropTypes } from 'react';
+
+import styles from '../Analysis.sass';
+
+import Card from '../../Base/Card';
+import HeaderBar from '../../Base/HeaderBar';
+import RegressionTable from './RegressionTable';
+import RegressionSummary from './RegressionSummary';
+
+export default class RegressionTableCard extends Component {
+  render() {
+    const { dependentVariableName, independentVariableNames, regressionResult, contributionToRSquared } = this.props;
+
+    return (
+      <Card>
+        <HeaderBar header={ <span>Explaining <strong className={ styles.dependentVariableTitle }>{ dependentVariableName }</strong></span> } />
+
+        <RegressionTable regressionResult={ regressionResult }/>
+        <RegressionSummary
+          dependentVariableName={ dependentVariableName }
+          independentVariableNames={ independentVariableNames }
+          regressionResult={ regressionResult }
+          contributionToRSquared={ contributionToRSquared }/>
+      </Card>
+    );
+  }
+}
+
+// RegressionTableCard.propTypes = {
+//   dependentVariableName: PropTypes.string,
+//   independentVariableNames: PropTypes.object.isRequired
+// }
