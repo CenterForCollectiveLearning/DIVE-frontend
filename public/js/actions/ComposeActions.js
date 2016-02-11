16d4060907a8542bcdb6ef6803c2cc98e8fa7d39
kevinzenghu <kzh@mit.edu>
2016-02-11 17:05:54 -0500

    Completed (untested) document CRUD functions

--- public/js/actions/ComposeActions.js
+++ public/js/actions/ComposeActions.js
@@ -1,6 +1,14 @@
 import {
   REQUEST_EXPORTED_VISUALIZATION_SPECS,
-  RECEIVE_EXPORTED_VISUALIZATION_SPECS
+  RECEIVE_EXPORTED_VISUALIZATION_SPECS,
+  REQUEST_DOCUMENT,
+  RECEIVE_DOCUMENT,
+  REQUEST_CREATE_DOCUMENT,
+  RECEIVE_CREATE_DOCUMENT,
+  REQUEST_UPDATE_DOCUMENT,
+  RECEIVE_UPDATE_DOCUMENT,
+  REQUEST_DELETE_DOCUMENT,
+  RECEIVE_DELETE_DOCUMENT
 } from '../constants/ActionTypes';
 
 import { fetch, pollForTask } from './api.js';
@@ -22,14 +30,6 @@ function receiveExportedVisualizationSpecsDispatcher(params, json) {
   }
 }
 
-function shouldFetchExportedVisualizationSpecs(state) {
-  const exportedVisualizationSpecs = state.exportedVisualizationSpecs;
-  if (exportedVisualizationSpecs.isFetching) {
-    return false;
-  }
-  return true;
-}
-
 export function fetchExportedVisualizationSpecs(projectId) {
   return dispatch => {
     dispatch(requestExportedVisualizationSpecsDispatcher());
@@ -42,11 +42,119 @@ export function fetchExportedVisualizationSpecs(projectId) {
   };
 }
 
-export function fetchExportedVisualizationSpecsIfNeeded(projectId) {
-  return (dispatch, getState) => {
-    if (shouldFetchExportedVisualizationSpecs(getState())) {
-      console.log('fetching');
-      return dispatch(fetchExportedVisualizationSpecs(projectId));
-    }
+function requestDocumentDispatcher(projectId, documentId) {
+  return {
+    type: REQUEST_DOCUMENT,
+    projectId: projectId,
+    documentId: documentId
   };
+}
+
+function receiveDocumentDispatcher(projectId, documentId, json) {
+  return {
+    type: RECEIVE_DOCUMENT,
+    projectId: projectId,
+    datasetId: datasetId,
+    'document': json.document,
+    receivedAt: Date.now(),
+  };
+}
+
+export function requestDocument(projectId, documentId) {
+  return (dispatch) => {
+    dispatch(requestDocumentDispatcher(projectId, documentId));
+    return fetch(`/compose/v1/document/${documentId}?project_id=${projectId}`)
+      .then(response => response.json())
+      .then(json => dispatch(receiveDocumentDispatcher(projectId, documentId, json)))
+  }
+}
+
+function requestCreateDocumentDispatcher(projectId) {
+  return {
+    type: REQUEST_CREATE_DOCUMENT,
+    projectId: projectId
+  };
+}
+
+function receiveCreateDocumentDispatcher(projectId, json) {
+  return {
+    type: RECEIVE_CREATE_DOCUMENT,
+    projectId: projectId,
+    documentId: json.id,
+    receivedAt: Date.now()
+  };
+}
+
+export function createNewDocument(projectId, content) {
+  const params = {
+    project_id: projectId,
+    content: content
+  }
+  return (dispatch) => {
+    dispatch(requestCreateDocumentDispatcher(projectId));
+    return fetch('/compose/v1/document', {
+      method: 'post',
+      body: JSON.stringify(params),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => response.json())
+      .then(json => dispatch(receiveCreateDocumentDispatcher(projectId, json)))
+  }
+}
+
+function requestUpdateDocumentDispatcher(projectId, documentId) {
+  return {
+    type: REQUEST_UPDATE_DOCUMENT,
+    projectId: projectId,
+    documentId: documentId
+  };
+}
+
+function receiveUpdateDocumentDispatcher(projectId, documentId) {
+  return {
+    type: REQUEST_UPDATE_DOCUMENT,
+    projectId: projectId,
+    datasetId: datasetId
+  };
+}
+
+export function updateDocument(projectId, documentId, content) {
+  const params = {
+    project_id: projectId,
+    content: content
+  }
+  return (dispatch) => {
+    dispatch(requestUpdateDocumentDispatcher(projectId, documentId));
+    return fetch(`/compose/v1/document/${documentId}`, {
+      method: 'put',
+      body: JSON.stringify(params),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => response.json())
+      .then(json => dispatch(receiveUpdateDocumentDispatcher(projectId, json)))
+  }
+}
+
+function requestDeleteDocumentDispatcher(projectId, documentId) {
+  return {
+    type: REQUEST_DELETE_DOCUMENT,
+    projectId: projectId,
+    documentId: documentId
+  };
+}
+
+function receiveDeleteDocumentDispatcher(projectId, json) {
+  return {
+    type: REQUEST_DELETE_DOCUMENT,
+    projectId: projectId,
+    doucmentid: json.id
+  };
+}
+
+export function deleteDocument(projectId, documentId) {
+  return (dispatch) => {
+    dispatch(requestDeleteDocumentDispatcher(projectId, documentId));
+    return fetch(`/compose/v1/document/${documentId}?project_id=${projectId}`, {
+      method: 'delete'
+    }).then(response => response.json())
+      .then(json => dispatch(receiveDeleteDocumentDispatcher(projectId, json)))
+  }
 }
