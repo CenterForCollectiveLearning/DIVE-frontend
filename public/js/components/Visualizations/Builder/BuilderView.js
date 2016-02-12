2583927320a407a9dfa002c2f20e3f52f1c0dc61
kevinzenghu <kzh@mit.edu>
2016-02-12 14:24:47 -0500

    Disabled save if spec exported (need to tie to visualization data)

--- public/js/components/Visualizations/Builder/BuilderView.js
+++ public/js/components/Visualizations/Builder/BuilderView.js
@@ -64,6 +64,8 @@ export class BuilderView extends Component {
 
   render() {
     const { visualization } = this.props;
+    console.log('Visualization: ', visualization);
+    const disabled = (visualization.isSaving || (!visualization.isSaving && visualization.exportedSpecId) || visualization.spec.exported) ? true : false;
     return (
       <VisualizationView visualization={ visualization }>
         <RaisedButton label="Back to Gallery" onClick={ this.onClickGallery } fullWidth={ true }/>
@@ -71,7 +73,7 @@ export class BuilderView extends Component {
           { visualization.isExporting && "Exporting..." }
           { !visualization.isExporting && "Share" }
         </RaisedButton>
-        <RaisedButton onClick={ this.saveVisualization } disabled={ (visualization.isSaving || (!visualization.isSaving && visualization.exportedSpecId)) ? true : false }>
+        <RaisedButton onClick={ this.saveVisualization } disabled={ disabled }>
           { !visualization.isSaving && visualization.exportedSpecId && <i className="fa fa-star"></i> }
           { !visualization.exportedSpecId && <i className="fa fa-star-o"></i> }
         </RaisedButton>
