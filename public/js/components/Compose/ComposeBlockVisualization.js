6c218f2acaa19c44ef27a4c3c0f91720e5bc79eb
Guru Mahendran <me@gurubavan.com>
2016-02-15 18:46:34 -0500

    compose-editor: visualization is now resizable
    but i took away the format switching functionality for now

--- public/js/components/Compose/ComposeBlockVisualization.js
+++ public/js/components/Compose/ComposeBlockVisualization.js
@@ -5,22 +5,47 @@ import { nextVisualizationFormat } from '../../actions/ComposeActions';
 
 import styles from './Compose.sass';
 
+import _ from 'underscore';
+import { ResizableBox } from 'react-resizable';
 import Visualization from '../Visualizations/Visualization';
 
 export default class ComposeBlockVisualization extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      resizeCounter: 0
+    }
+
+    this.onResize = this.onResize.bind(this);
+  }
+
+  onResize(event, { element, size }) {
+    this.setState({ resizeCounter: this.state.resizeCounter + 1 });
+  }
+
+  onClickVisualization(e) {
+    this.props.nextVisualizationFormat(this.props.spec.id);
+  }
+
   render() {
     const { spec, updatedAt, nextVisualizationFormat } = this.props;
     return (
       <div className={ styles.composeBlockVisualization }>
-        <Visualization
-          chartId={ `full-compose-visualization-${ spec.id }-${ updatedAt }` }
-          containerClassName={ styles.fullComposeVisualization }
-          visualizationTypes={ spec.vizTypes }
-          spec={ spec }
-          data={ spec.data }
-          isMinimalView={ false }
-          showHeader={ false }
-          onClick={ nextVisualizationFormat } />
+        <ResizableBox
+          className={ styles.resizableContainer }
+          onResize={ _.debounce(this.onResize, 500) }
+          width={ 400 }
+          height={ 200 }>
+          <Visualization
+            chartId={ `full-compose-visualization-${ spec.id }-${ updatedAt }-${ this.state.resizeCounter }` }
+            containerClassName={ styles.fullComposeVisualization }
+            visualizationTypes={ spec.vizTypes }
+            spec={ spec }
+            data={ spec.data }
+            isMinimalView={ false }
+            showHeader={ false }/>
+        </ResizableBox>
       </div>
     );
   }
