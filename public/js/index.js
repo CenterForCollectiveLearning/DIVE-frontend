01eefdad962173c8a2bf26ef8ad319555a18bd4f
Guru Mahendran <me@gurubavan.com>
2016-04-21 15:44:35 -0400

    analytics: adjust page view event

--- public/js/index.js
+++ public/js/index.js
@@ -16,7 +16,7 @@ EventPluginHub.injection.injectEventPluginsByName({ TapEventPlugin });
 
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
-history.listen(location => amplitude.logEvent(`Viewed: ${ location.pathname }`));
+history.listen(location => amplitude.logEvent('Page View', { pathname: location.pathname });
 
 if ((window.__env.NODE_ENV == 'STAGING' || window.__env.NODE_ENV == 'PRODUCTION') && window.localStorage['powerAuth'] != 'true') {
   const password = prompt('Please enter your password to view this page.', '');
