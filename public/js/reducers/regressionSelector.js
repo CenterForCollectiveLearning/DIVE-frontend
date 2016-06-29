ebc7bb1913798a254a6d8dba5a8d68e6f261c0f0
Suzanne Wang <suzanneswang@gmail.com>
2016-06-29 14:32:14 -0400

    add default regression

--- public/js/reducers/regressionSelector.js
+++ public/js/reducers/regressionSelector.js
@@ -25,7 +25,7 @@ const regressionModes = [ {
 
 const baseState = {
   fieldProperties: [],
-  regressionType: null,
+  regressionType: 'linear',
   dependentVariableId: null,
   independentVariableIds: [],
   regressionResult: {
