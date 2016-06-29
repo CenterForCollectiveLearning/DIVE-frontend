da9a53d808eba400311b7157ad861b7fcfbf1aa0
kevinzenghu <kzh@mit.edu>
2016-06-29 14:34:05 -0400

    Fixing pushState import

--- public/js/components/Analysis/Comparison/ComparisonPage.js
+++ public/js/components/Analysis/Comparison/ComparisonPage.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { pushState } from 'redux-react-router';
 import styles from '../Analysis.sass';
 
 import ComparisonSidebar from './ComparisonSidebar';
@@ -21,4 +20,4 @@ function mapStateToProps(state) {
   return {};
 }
 
-export default connect(mapStateToProps, { pushState })(ComparisonPage);
+export default connect(mapStateToProps, { })(ComparisonPage);
