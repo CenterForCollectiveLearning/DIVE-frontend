4af1571052a937167b98b9482e7b877dc6a84b51
Suzanne Wang <suzanneswang@gmail.com>
2016-07-01 16:05:50 -0400

    choose regression based on variable

--- public/js/components/Analysis/Regression/RegressionPage.js
+++ public/js/components/Analysis/Regression/RegressionPage.js
@@ -3,14 +3,30 @@ import { connect } from 'react-redux';
 
 import { selectDependentVariable, selectRegressionType } from '../../../actions/RegressionActions';
 
+function dependentVariableToRegressionType(type) {
+  const dvToType = {
+    q: 'linear',
+    c: 'logistic',
+    t: 'linear'
+  }
+  return dvToType[type];
+}
+
 export class RegressionPage extends Component {
   componentWillMount() {
-    this.props.selectDependentVariable(this.props.params.dependentVariable);
+    const { selectDependentVariable, params, fieldProperties, selectRegressionType} = this.props;
+    selectDependentVariable(params.dependentVariable);
+    const dependentVariable = fieldProperties.items.filter((property) => property.id == params.dependentVariable)
+    if(dependentVariable.length > 0) {
+      selectRegressionType(dependentVariableToRegressionType(dependentVariable[0].generalType));
+    }
   }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.params.dependentVariable != nextProps.params.dependentVariable) {
       this.props.selectDependentVariable(nextProps.params.dependentVariable);
+      const dependentVariable = this.props.fieldProperties.items.filter((property) => property.id == nextProps.params.dependentVariable)
+      this.props.selectRegressionType(dependentVariableToRegressionType(dependentVariable[0].generalType))
     }
   }
 
@@ -18,7 +34,10 @@ export class RegressionPage extends Component {
 }
 
 function mapStateToProps(state) {
-  return {};
+  const { fieldProperties } = state;
+  return {
+    fieldProperties
+  };
 }
 
-export default connect(mapStateToProps, { selectDependentVariable })(RegressionPage);
+export default connect(mapStateToProps, { selectDependentVariable, selectRegressionType })(RegressionPage);
