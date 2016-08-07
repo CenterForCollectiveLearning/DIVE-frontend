7070736f02e8bd0147f3f68a6e7d5c488165dc00
kevinzenghu <kzh@mit.edu>
2016-08-07 14:44:43 -0400

    Fixing _redirects copying

--- webpack.config.js
+++ webpack.config.js
@@ -35,9 +35,9 @@ module.exports = {
     }),
     new CopyWebpackPlugin([
       { from: './src/index.html', to: './index.html' },
-      { from: './src/404.html', to: './404.html' },      
+      { from: './src/404.html', to: './404.html' },
       { from: './src/assets', to: './assets'},
-      { from: './_redirects', to: './_redirects'}
+      { from: './_redirects' }
     ])
   ],
   module: {
