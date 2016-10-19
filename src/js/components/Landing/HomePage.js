84fb2c6c2320cba4acb7be6286591f52d02eb6c5
kevinzenghu <kzh@mit.edu>
2016-10-19 10:38:52 -0400

    Adding relevant copy and e-mail.

--- src/js/components/Landing/HomePage.js
+++ src/js/components/Landing/HomePage.js
@@ -60,7 +60,7 @@ export class HomePage extends Component {
               <span>Easy and powerful data exploration</span>
             </div>
             <div className={ styles.secondaryCopy }>
-              DIVE lets
+              DIVE lets you turn data into stories within minutes, without writing a single line of code
             </div>
             <div className={ styles.ctaContainer }>
               <RaisedButton
@@ -94,7 +94,7 @@ export class HomePage extends Component {
               <div className={ styles.sectionContent }>
                 <img className={ styles.gif } src="/assets/images/ingest.gif"/>
                 <div className={ styles.textBox }>
-                  <p>DIVE automatically recommends visualizations based on the fields you are interested in. No more building visualizations from scratch. You can sort visualizations based on effectiveness, expressiveness, and statistical properties like correlation, entropy, and gini.</p>
+                  <p>DIVE intelligently samples your data to <b>infer the types of your fields and the structure of your datasets</b> to best inform your visualizations and statistical analyses. DIVE works with human-interpretable data types, not machine types, to best match your domain knowledge.</p>
                 </div>
               </div>
             </div>
@@ -104,7 +104,7 @@ export class HomePage extends Component {
               <div className={ styles.sectionContent }>
                 <img className={ styles.gif } src="/assets/images/visualization.gif"/>
                 <div className={ styles.textBox }>
-                  <p>DIVE automatically recommends visualizations based on the fields you are interested in. No more building visualizations from scratch. You can sort visualizations based on effectiveness, expressiveness, and statistical properties like correlation, entropy, and gini.</p>
+                  <p>Instead of learning syntax or specifying visual mappings, simply <b>select the fields you're interested in and DIVE will recommend relevant visualizations.</b> No more building visualizations from scratch. You can <b>sort visualizations</b> based on effectiveness, expressiveness, and statistical properties like correlation, entropy, and gini.</p>
                 </div>
               </div>
             </div>
@@ -114,7 +114,7 @@ export class HomePage extends Component {
               <div className={ styles.sectionContent }>
                 <img className={ styles.gif } src="/assets/images/analysis.gif"/>
                 <div className={ styles.textBox }>
-                  <p>DIVE lowers the barrier to understanding data so you can focus on interpreting results, not technical minutiae. Run ANOVA to compare group means, build regressions to explore relationships between fields, and more, all without writing a single line of code. </p>
+                  <p>DIVE <b>lowers the barrier to running statistical analyses so you can focus on interpreting results, not technical minutiae</b>. Run ANOVA to compare group means, build regressions to explore relationships between fields, and more, all without writing a single line of code. </p>
                 </div>
               </div>
             </div>
@@ -122,10 +122,10 @@ export class HomePage extends Component {
             <div className={ styles.section + ' ' + styles.contentSection }>
               <div className={ styles.sectionHeader }>WYSIWYG Visual Narratives</div>
               <div className={ styles.sectionContent }>
-                <div className={ styles.textBox }>
-                  <p></p>
-                </div>
                 <img className={ styles.gif } src="/assets/images/compose.gif"/>
+                <div className={ styles.textBox }>
+                  <p>With the visualizations and analyses you've saved, <b>construct visual stories with a what-you-see-is-what-you-get editor</b>. Share stories with <b>interactive content linked to dynamic data.</b> If this doesn't suit you, yes, we are working on powerpoint export ☺. </p>
+                </div>
               </div>
             </div>
 
@@ -133,7 +133,7 @@ export class HomePage extends Component {
               <div className={ styles.sectionHeader }>DIVE Development Team</div>
               <div className={ styles.sectionContent }>
                 <div className={ styles.textBox }>
-                  <p>DIVE is built by <a target="_blank" href="https://twitter.com/KevinZengHu">Kevin Hu</a> and <a target="_blank" href="https://twitter.com/cesifoti">César Hidalgo</a> in the <a target="_blank" href="http://macro.media.mit.edu">Macro Connections Group</a> at the <a target="_blank" href="http://media.mit.edu">MIT Media Lab</a></p>
+                  <p>DIVE is built by <a target="_blank" href="https://twitter.com/KevinZengHu">Kevin Hu</a> and <a target="_blank" href="https://twitter.com/cesifoti">César Hidalgo</a> in the <a target="_blank" href="http://macro.media.mit.edu">Macro Connections Group</a> at the <a target="_blank" href="http://media.mit.edu">MIT Media Lab</a>. To give feedback, compliments, or complaints, please e-mail us at <a href="mailto:dive@media.mit.edu" target="_top">dive@media.mit.edu</a>.</p>
                 </div>
                 <div className={ styles.mugshotBox }>
                   <div className={ styles.mugshots }>
