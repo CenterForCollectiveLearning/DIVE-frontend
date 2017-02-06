f723d836b0507dd1f6045ad8a8fe52bcd2a08dc3
kzh <kzh@mit.edu>
2017-02-06 18:43:57 -0500

    Fix loader text. Change Roboto font.

--- src/js/components/Base/Loader.js
+++ src/js/components/Base/Loader.js
@@ -15,14 +15,19 @@ export default class Loader extends Component {
     const parsedText = (Array.isArray(text)) ? text[0] : text;
     return (
       <div className={ styles.loader }>
-        { !error && <div className="pt-spinner pt-medium">
-          <div className="pt-spinner-svg-container">
-            <svg viewBox="0 0 100 100">
-              <path className="pt-spinner-track" d="M 50,50 m 0,-44.5 a 44.5,44.5 0 1 1 0,89 a 44.5,44.5 0 1 1 0,-89"></path>
-              <path className="pt-spinner-head" d="M 94.5 50 A 44.5 44.5 0 0 0 50 5.5"></path>
-            </svg>
+        { !error &&
+          <div className="pt-spinner pt-medium">
+            <div className="pt-spinner-svg-container">
+              <svg viewBox="0 0 100 100">
+                <path className="pt-spinner-track" d="M 50,50 m 0,-44.5 a 44.5,44.5 0 1 1 0,89 a 44.5,44.5 0 1 1 0,-89"></path>
+                <path className="pt-spinner-head" d="M 94.5 50 A 44.5 44.5 0 0 0 50 5.5"></path>
+              </svg>
+            </div>
           </div>
-        </div> }
+        }
+        {
+          (!error && text) && <div className={ styles.text }>{ text }</div>
+        }
         { error &&
           <NonIdealState
             title={ text }
