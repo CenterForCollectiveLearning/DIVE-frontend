4d0ac8e3ad9f625d1cbf86e63ee77cc87a7140e0
kzh <kzh@mit.edu>
2017-03-10 18:58:26 -0500

    Not found page located

--- src/js/components/Landing/NotFoundPage.js
+++ src/js/components/Landing/NotFoundPage.js
@@ -5,18 +5,55 @@ import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 
 import Link from '../Base/Link';
+import Footer from './Footer';
 import HomePage from './HomePage';
 import { wipeProjectState } from '../../actions/ProjectActions';
 
+import { Position, Toaster, Button, Intent, NonIdealState } from '@blueprintjs/core';
+
+import Logo from '../../../assets/DIVE_logo_white.svg?name=Logo';
 
 export class NotFoundPage extends Component {
+  goHome = () => {
+    this.props.push(`/`);
+  }
+
   render() {
-    const { } = this.props;
+    const { user } = this.props;
     return (
       <DocumentTitle title='DIVE | Error'>
         <div className={ styles.fillContainer + ' ' + styles.notFoundPage }>
-          Test
-          <div>Click here to go back to DIVE</div>
+          <nav className={ 'pt-navbar pt-dark pt-fixed-top ' + styles.header + ' ' + styles.opaque }>
+            <div className="pt-navbar-group pt-align-left">
+              <div className={ 'pt-navbar-heading ' + styles.logoContainer } onClick={ this.goHome }>
+                <Logo className={ styles.logo } />
+                <div className={ styles.logoText }>DIVE</div>
+              </div>
+            </div>
+            <div className="pt-navbar-group pt-align-right">
+              { (user.id && !user.anonymous) &&
+                <div className={ styles.rightButtons }>
+                  <Link className="pt-button pt-minimal pt-icon-projects" route="/projects">Projects</Link>
+                  <span className="pt-navbar-divider"></span>
+                  <div className="pt-button pt-minimal pt-icon-log-out" onClick={ this.props.logoutUser }>Log Out of { user.username }</div>
+                </div>
+              }
+              { (user.anonymous || !user.id) &&
+                <div className={ styles.rightButtons }>
+                  <Link className="pt-button pt-minimal pt-icon-log-in" route="/auth/login">Log In</Link>
+                  <Link className="pt-button pt-minimal pt-icon-user" route="/auth/register">Register</Link>
+                </div>
+              }
+            </div>
+          </nav>
+          <NonIdealState
+            className={ styles.centeredFill + ' ' + styles.notFoundPageContent }
+            title='Not Found'
+            description={ <p className='pt-running-text'>The page you requested could not be located.</p> }
+            action={ <span className={ styles.link } onClick={ this.goHome }>Click here to return to DIVE.</span> }
+            visual='error'
+          />
+          <Footer />
         </div>
       </DocumentTitle>
     );
@@ -24,7 +61,8 @@ export class NotFoundPage extends Component {
 }
 
 function mapStateToProps(state) {
-  return { };
+  const { user } = state;
+  return { user };
 }
 
 export default connect(mapStateToProps, { push })(NotFoundPage);
